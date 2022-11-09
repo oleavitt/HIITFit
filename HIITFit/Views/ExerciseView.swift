44a6072d2765a749bdbd3b0d51f55f4260eec9e6
Oren Leavitt <oleavitt@ix.netcom.com>
2022-11-09 14:50:05 -0600

    Add basic header view

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -38,7 +38,24 @@ struct ExerciseView: View {
     let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
     let index: Int
     var body: some View {
-        Text(exerciseNames[index])
+        VStack {
+            VStack {
+                Text(exerciseNames[index])
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Image(systemName: "1.circle")
+                    Image(systemName: "2.circle")
+                    Image(systemName: "3.circle")
+                    Image(systemName: "4.circle")
+                }
+                .font(.title2)
+            }
+          Text("Video player")
+          Text("Timer")
+          Text("Start/Done button")
+          Text("Rating")
+          Text("History button")
+        }
     }
 }
 
